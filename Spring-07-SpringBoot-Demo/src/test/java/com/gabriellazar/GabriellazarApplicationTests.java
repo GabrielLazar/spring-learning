a779413f1577963f0809c6a50aa82f1bdbb2bfed
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-10-27 23:50:24 -0500

    Spring starter

---
+++ Spring-07-SpringBoot-Demo/src/test/java/com/gabriellazar/GabriellazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabriellazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
