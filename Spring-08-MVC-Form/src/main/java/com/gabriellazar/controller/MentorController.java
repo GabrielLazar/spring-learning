38c17bb262d91d3ef91f56e6798257af32af5880
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-11-18 00:34:46 -0600

    linking form to confirmation

--- Spring-08-MVC-Form/src/main/java/com/gabriellazar/controller/MentorController.java
+++ Spring-08-MVC-Form/src/main/java/com/gabriellazar/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.gabriellazar.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -20,5 +22,11 @@ public class MentorController {
         List<String> list = Arrays.asList("B1","B2","B3","B4");
         model.addAttribute("batchList",list);
         return "mentor/register";
+    }
+
+    @PostMapping("confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+        System.out.println(mentor.toString());
+        return "mentor/confirmation";
     }
 }
