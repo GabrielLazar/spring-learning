6a7351bc05d6e13c94d8bcaa7666d61215b43833
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-11-18 20:13:37 -0600

    employee list, create employee, html bootstrap validation

--- Spring-08-MVC-Form/src/main/java/com/gabriellazar/controller/EmployeeController.java
+++ Spring-08-MVC-Form/src/main/java/com/gabriellazar/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.gabriellazar.controller;
 
+import com.gabriellazar.dataGenerator.DataGenerator;
 import com.gabriellazar.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/employee")
@@ -14,6 +20,17 @@ public class EmployeeController {
     public String employeeCreate(Model model){
 
         model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.getStateList());
         return "/employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String getEmployeeList(@ModelAttribute("employee") Employee employee, Model model){
+
+        model.addAttribute("employees", Arrays.asList(employee));
+        int birthYear = LocalDate.parse(employee.getBirthday()).getYear();
+        model.addAttribute("age",LocalDate.now().getYear() - birthYear);
+
+        return "/employee/employee-list";
     }
 }
