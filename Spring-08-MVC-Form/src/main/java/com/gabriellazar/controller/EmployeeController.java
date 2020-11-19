fc21895ad1068022891b990fa3a554a67227b056
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-11-18 18:41:55 -0600

    employee

---
+++ Spring-08-MVC-Form/src/main/java/com/gabriellazar/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.gabriellazar.controller;
+
+import com.gabriellazar.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("register")
+    public String employeeCreate(Model model){
+
+        model.addAttribute("employee", new Employee());
+        return "/employee/employee-create";
+    }
+}
