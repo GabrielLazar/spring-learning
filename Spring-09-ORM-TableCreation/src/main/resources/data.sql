b7cd8a5e610d31d877e5ae06a48ad7552ce24786
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-12-12 12:45:01 -0600

    @Entity and @Id,@GeneratedValue in ORM

---
+++ Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO student(id,first_name,last_name,email) VALUES(1,'Mike','Smith','msmith@google.com');
+INSERT INTO student(id,first_name,last_name,email) VALUES(2,'Adam','Smith','asmith@google.com');
+INSERT INTO student(id,first_name,last_name,email) VALUES(3,'Tom','Jones','tjones@google.com');
+INSERT INTO student(id,first_name,last_name,email) VALUES(4,'James','Bond','jbond@google.com');
