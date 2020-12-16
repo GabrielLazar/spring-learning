ab941bf7ebb9d6cd7daa6c244ed67ae9ec192fc9
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-12-15 19:10:24 -0600

    @OneToOne and @JoinColumn

--- Spring-09-ORM-Mapping-OneToOne/src/main/resources/data.sql
+++ Spring-09-ORM-Mapping-OneToOne/src/main/resources/data.sql
@@ -5,7 +5,7 @@ insert into departments values (4,'Furniture','Home');
 insert into departments values (5,'Computers','Electronics');
 insert into departments values (6,'Device Repair','Electronics');
 
-insert into employees values (1, 'Berrie', 'Manueau', 'bmanueau0@dion.ne.jp', '2006-04-20',  'F', 154864);
-insert into employees values (2, 'Aeriell', 'McNee', 'amcnee1@google.es', '2009-01-26',  'F', 56752);
-insert into employees values (3, 'Sydney', 'Symonds', 'ssymonds2@hhs.gov', '2010-05-17',  'F', 95313);
-insert into employees values (5, 'Feliks', 'Morffew', 'fmorffew4@a8.net', '2003-01-14', 'M', 55307);
+insert into employees values (1, 'Berrie', 'Manueau', 'bmanueau0@dion.ne.jp', '2006-04-20',  'F', 154864,1);
+insert into employees values (2, 'Aeriell', 'McNee', 'amcnee1@google.es', '2009-01-26',  'F', 56752,2);
+insert into employees values (3, 'Sydney', 'Symonds', 'ssymonds2@hhs.gov', '2010-05-17',  'F', 95313,3);
+insert into employees values (5, 'Feliks', 'Morffew', 'fmorffew4@a8.net', '2003-01-14', 'M', 55307,4);
