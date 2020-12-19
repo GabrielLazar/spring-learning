c2365740767dbec7e598df36567f4ab0109886dd
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-12-19 09:36:20 -0600

    Many to many

---
+++ Spring-09-ORM-ManyToMany/src/test/java/com/gabriellazar/orm/OrmManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellazar.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
