6b449d6ac7ed24e9f8cfa40259ff825a09b4dbb2
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-12-23 19:56:51 -0600

    named queries jpql

--- Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/gabriellazar/ormjpql/repository/EmployeeRepository.java
+++ Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/gabriellazar/ormjpql/repository/EmployeeRepository.java
@@ -3,10 +3,12 @@ package com.gabriellazar.ormjpql.repository;
 
 import com.gabriellazar.ormjpql.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -40,5 +42,15 @@ public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
     @Query(value = "SEELCT * FROM employees WHERE salary = ?1",nativeQuery = true)
     Employee readEmployeeBySalary(int salary);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Employee e SET e.email = :email WHERE e.id = :id")
+    void UpdateEmployeeJPQL(@Param("id") Integer id, @Param("email") String email);
+    
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE employees  SET email = :email WHERE id = :id",nativeQuery = true)
+    void UpdateEmployeeJPQLNative(@Param("id") Integer id, @Param("email") String email);
 
 }
--- Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/gabriellazar/ormjpql/repository/RegionRepository.java
+++ Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/gabriellazar/ormjpql/repository/RegionRepository.java
@@ -2,6 +2,7 @@ package com.gabriellazar.ormjpql.repository;
 
 import com.gabriellazar.ormjpql.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,10 @@ import java.util.List;
 @Repository
 public interface RegionRepository extends JpaRepository<Region,Long> {
 
+    List<Region> retrieveRegionByCountry(String country);
 
+    List<Region> getAllRegions();
+
+    @Query(nativeQuery = true)
+    List<Region> getRegionLike(String pattern);
 }
