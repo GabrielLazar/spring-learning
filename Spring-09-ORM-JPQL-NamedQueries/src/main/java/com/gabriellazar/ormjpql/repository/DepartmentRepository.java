ee9189a7fde3c6ff197ca0c7628306bf183f60eb
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-12-23 19:06:43 -0600

    parameter query and native query

--- Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/gabriellazar/ormjpql/repository/DepartmentRepository.java
+++ Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/gabriellazar/ormjpql/repository/DepartmentRepository.java
@@ -2,11 +2,18 @@ package com.gabriellazar.ormjpql.repository;
 
 
 import com.gabriellazar.ormjpql.entity.Department;
+import com.gabriellazar.ormjpql.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
 
+    @Query("SELECT d FROM Department d WHERE d.division IN ?1")
+    List<Department> getDepartmentByDivisionIn(List<String> division);
+
+    @Query("SELECT d FROM Department d ORDER BY d.division")
+    List<Employee> getDepartmentByDivisionOrderAsc();
 
 }
--- Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/gabriellazar/ormjpql/repository/EmployeeRepository.java
+++ Spring-09-ORM-JPQL-NamedQueries/src/main/java/com/gabriellazar/ormjpql/repository/EmployeeRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
@@ -29,5 +31,14 @@ public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
     @Query("SELECT e FROM Employee e WHERE e.first_name = :firstName OR e.salary = :salary")
     Employee getEmployeeBySFirstNameOrSalary(@Param("salary") int salary,@Param("firstName") String firstName);
+
+    @Query("SELECT E FROM Employee e WHERE e.salary > ?1")
+    List<Employee> getEmployeeBySalaryGreaterThan(Integer salary);
+
+    @Query("SELECT E FROM Employee e WHERE e.salary BETWEEN ?1 AND ?2")
+    List<Employee> getEmployeeBySalaryBetween(int salary1, int salary2);
+
+    @Query(value = "SEELCT * FROM employees WHERE salary = ?1",nativeQuery = true)
+    Employee readEmployeeBySalary(int salary);
 
 }
