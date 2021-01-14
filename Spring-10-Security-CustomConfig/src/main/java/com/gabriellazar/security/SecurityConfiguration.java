a0f64cd532923e8ee3f397700ab797faa6089684
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-01-13 20:29:42 -0600

    using security config with passwordEncoder

--- Spring-10-Security-CustomConfig/src/main/java/com/gabriellazar/security/SecurityConfiguration.java
+++ Spring-10-Security-CustomConfig/src/main/java/com/gabriellazar/security/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.gabriellazar.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,11 +16,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests() //request should be authorized
+                .anyRequest().authenticated() //incoming request be authenticated
+                .and()
+                .httpBasic();  //perform basic http authentication
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+
+       auth.inMemoryAuthentication()
+               .withUser("admin").password(getPasswordEncoder().encode("admin")).roles("ADMIN")
+               .and()
+               .withUser("Gabriel").password(getPasswordEncoder().encode("qwerty")).roles("USER");
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
