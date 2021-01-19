5e5166b10b5977a4e508b504e6f0c927cb3b0c7e
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-01-18 22:35:46 -0600

    User Service

---
+++ Spring-10-Security-CustomConfig/src/main/java/com/gabriellazar/security/UserPrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.gabriellazar.security;
+
+import com.gabriellazar.entity.User;
+import com.gabriellazar.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        return  new UserPrincipal(user);
+    }
+}
